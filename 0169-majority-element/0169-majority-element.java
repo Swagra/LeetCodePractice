00846641f424cd75d1a17233c2872cb752d27cbd
Swagra <bibiswagrats@gmail.com>
2025-09-30 15:15:57 +0530

    Time: 2594 ms (5.12%), Space: 53.2 MB (39.84%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int k = nums[0];
+        int max = 0;
+       for(int i=0;i<nums.length;i++) {
+        int count=1;
+        for(int j=i+1;j<nums.length;j++) {
+            if(nums[i] == nums[j]) {
+                count++;
+            }
+        }
+        if(count > max) {
+            max = count;
+            k = nums[i];
+        }
+       } 
+       return k;
+    }
+}
